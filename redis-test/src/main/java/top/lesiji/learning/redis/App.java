63d654927e07527564fe18221160742493017815
peng xiwen <xiwen.peng@lesiji.top>
2017-09-05 18:54:42 +0800

    maven config for redis-test

---
+++ redis-test/src/main/java/top/lesiji/learning/redis/App.java
@@ -0,0 +1,13 @@
+package top.lesiji.learning.redis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
