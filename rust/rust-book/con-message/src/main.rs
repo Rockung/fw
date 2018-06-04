8948bb2b12116f2cd20ff202cc844fb6a4d4fbe1
xiwen, peng <xiwen.peng@lesiji.top>
2018-06-04 19:52:33 +0800

    send/receive in rust

---
+++ rust/rust-book/con-message/src/main.rs
@@ -0,0 +1,28 @@
+use std::thread;
+use std::sync::mpsc; // multiple producer single consumer
+use std::time::Duration;
+
+fn main() {
+    // transmitter, receiver
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("Hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    // rx.recv().unwrap()
+    // rx as an iterator
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
