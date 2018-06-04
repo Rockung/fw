eae5ae0f909b90865df984cc63fbbca7079956cd
xiwen, peng <xiwen.peng@lesiji.top>
2018-06-04 19:56:06 +0800

    clone the transmitter

--- rust/rust-book/con-message/src/main.rs
+++ rust/rust-book/con-message/src/main.rs
@@ -6,6 +6,21 @@ fn main() {
     // transmitter, receiver
     let (tx, rx) = mpsc::channel();
 
+    let tx1 = mpsc::Sender::clone(&tx);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("Hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    
     thread::spawn(move || {
         let vals = vec![
             String::from("Hi"),
