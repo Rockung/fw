031bace55f4c8a5f641713c466c3bf9facf560d3
xiwen, peng <xiwen.peng@lesiji.top>
2018-12-20 08:53:07 +0800

    mock http

--- fe/umijs/antd-course/src/model/puzzlecards.js
+++ fe/umijs/antd-course/src/model/puzzlecards.js
@@ -15,7 +15,7 @@ export default {
   effects: {
     *queryInitCards(_, sagaEffects) {
       const { call, put } = sagaEffects;
-      const endPointURI = 'http://114.116.89.193:8999/test/cardList';
+      const endPointURI = 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke';
 
       const puzzle = yield call(request, endPointURI);
       yield put({ type: 'addNewCard', payload: puzzle });
