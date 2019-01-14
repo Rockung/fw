0aea8435455fc9d82db3f067d4048f6f2a014741
xiwen, peng <xiwen.peng@lesiji.top>
2019-01-14 10:18:36 +0800

    assets management

--- fe/webpack-demo/src/index.js
+++ fe/webpack-demo/src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.svg';
 
 function component() {
   var element = document.createElement('div');
   
   // Loash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
+  var myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+  
   return element;
 }
 
