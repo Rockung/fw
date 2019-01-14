313beedd5be6190c8c155e38af449eef723c85c3
xiwen, peng <xiwen.peng@lesiji.top>
2019-01-14 11:11:36 +0800

    seperate outputs

--- fe/webpack-demo/src/index.js
+++ fe/webpack-demo/src/index.js
@@ -1,18 +1,18 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.svg';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
   
   // Loash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
   
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  
+  element.appendChild(btn);
+
   return element;
 }
 
