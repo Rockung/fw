0d5a44f3469f07e1f2c89c2bb9dce9a20fb0cf6f
Rockung <rockung@lesiji.top>
2019-03-08 22:13:14 +0800

    some docs about webrtc

--- nodejs/es6/index.js
+++ nodejs/es6/index.js
@@ -1,3 +1,17 @@
+import { Socket } from 'phoenix';
+
+window.onload = function() {
+  let b = document.getElementById('connect');
+  b.addEventListener('click',() => {
+    let socket = new Socket("wss://beam.oorja.io/socket", 
+      {
+        params: {userToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VySWQiOiJbYW5vbl1XWWNISjhaUVE3RGQiLCJ2IjoxLCJpYXQiOjE1NTE5NTc5NzA0Njl9.rJgwXzX2HP9u-CtIUAS9QGmxWsdOJac1RHFa9r4kvv9qf-zjPGVuVKGb2wqB5H1f_N6oD-dMTMV-PzFbF8puXg"}
+      }
+    );
+    socket.connect();
+  }, false);
+}
+
 const helloWorld = () => {
     console.log("Hello! We are all set!");
     console.log("Arrow functions are working");
